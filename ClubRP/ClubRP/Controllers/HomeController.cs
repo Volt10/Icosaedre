88f0b26040cf7fdde73336390344aad1f6a9e426
Volten <davidbisaillon@hotmail.com>
2015-09-15 17:47:18 -0400

    no message

---
+++ ClubRP/ClubRP/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClubRP.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            ViewBag.Titre = "Acceuil";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Titre = "A propos";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Titre = "Nous contacter";
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Materiel()
+        {
+
+            return View();
+        }
+    }
+}
